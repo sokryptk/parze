2aee64f2f6928e6ec595d010c0df7e4c0de1afd0
krypticallusion <krypticallusion@gmail.com>
2023-02-18 07:14:53 +0530

    parze: add support for a basic parser
    Signed-off-by: krypticallusion <krypticallusion@gmail.com>

--- src/main.zig
+++ src/main.zig
@@ -3,41 +3,175 @@ const std = @import("std");
 const delegate = *const fn () void;
 const noCommandFound = "parzer-command-not-found";
 
-pub fn Parser() type {
+pub const Options = struct {};
+pub const Err = error{
+    Bruh,
+};
+
+pub fn Result(comptime flags: type) type {
     return struct {
-        allocator: std.mem.Allocator,
-        middlewares: std.StringHashMap(delegate),
-        const Self = @This();
+        flags: flags,
+        arguments: [][]const u8,
+    };
+}
 
-        pub fn init(allocator: std.mem.Allocator) Self {
-            return .{
-                .allocator = allocator,
-                .middlewares = std.StringHashMap(delegate).init(allocator),
-            };
+pub const Parser = struct {
+    allocator: std.mem.Allocator,
+    middlewares: std.StringHashMap(delegate),
+    const Self = @This();
+
+    pub fn init(allocator: std.mem.Allocator) Self {
+        return .{
+            .allocator = allocator,
+            .middlewares = std.StringHashMap(delegate).init(allocator),
+        };
+    }
+
+    pub fn onCommand(self: *Self, cmd: []const u8, on: delegate) !void {
+        try self.middlewares.put(cmd, on);
+    }
+
+    pub fn onDefault(self: *Self, on: delegate) !void {
+        try self.middlewares.put(noCommandFound, on);
+    }
+
+    // support parsing flags with = and Space delimiter.
+    // -a index.js , -a=index.js, --append=index.js, --append index.js
+    pub fn parse(self: Self, comptime flags: type, opts: Options) !Result(flags) {
+        _ = opts;
+
+        if (!comptime isDefaulted(flags)) {
+            return Err.Bruh;
         }
 
-        pub fn onCommand(self: *Self, cmd: []const u8, on: delegate) !void {
-            try self.middlewares.put(cmd, on);
-        }
+        const args = try std.process.argsAlloc(self.allocator);
+        defer std.process.argsFree(self.allocator, args);
 
-        pub fn onDefault(self: *Self, on: delegate) !void {
-            try self.middlewares.put(noCommandFound, on);
-        }
+        var i: usize = 0;
+        var res: flags = .{};
 
-        pub fn run(self: *Self) !void {
-            const args = try std.process.argsAlloc(self.allocator);
-            defer std.process.argsFree(self.allocator, args);
+        while (i < args.len) : (i += 1) {
+            const arg = args[i];
 
-            for (args) |arg| {
-                if (self.middlewares.get(arg)) |del| {
-                    return del();
+            if (std.mem.startsWith(u8, arg, "--")) {
+                // long flags
+                const prefix = arg[2..];
+
+                inline for (@typeInfo(flags).Struct.fields) |field| {
+                    if (std.mem.eql(u8, prefix, field.name)) {
+                        switch (@TypeOf(@field(res, field.name))) {
+                            bool => @field(res, field.name) = true,
+
+                            usize, []const u8 => |dtype| {
+                                if (i >= args.len - 1) {
+                                    // last element
+                                    return Err.Bruh;
+                                }
+
+                                @field(res, field.name) = switch (dtype) {
+                                    usize => try std.fmt.parseInt(
+                                        usize,
+                                        args[i + 1],
+                                        10,
+                                    ),
+                                    else => args[i + 1],
+                                };
+                            },
+
+                            else => unreachable,
+                        }
+                    }
                 }
-            }
+            } else if (std.mem.startsWith(u8, arg, "-")) {
+                // short flags can be merged together like -abc
+                // these can also contain values
+                const prefix = arg[1..];
 
-            // no args matched to command
-            if (self.middlewares.get(noCommandFound)) |del| {
-                del();
+                for (prefix) |short, index| {
+                    inline for (@typeInfo(flags).Struct.fields) |field| {
+                        const flag = extractFlag(short, flags);
+
+                        if (flag == null) {
+                            return Err.Bruh;
+                        }
+
+                        if (std.mem.eql(u8, field.name, flag.?)) {
+                            switch (@TypeOf(@field(res, field.name))) {
+                                bool => @field(res, field.name) = true,
+                                u64, []const u8 => |dtype| {
+                                    // it should absolutely be the last short to hold a non-boolean value
+                                    if (index != prefix.len - 1) {
+                                        return Err.Bruh;
+                                    }
+
+                                    if (i >= args.len - 1) {
+                                        return Err.Bruh;
+                                    }
+
+                                    @field(res, field.name) = switch (dtype) {
+                                        u64 => try std.fmt.parseInt(
+                                            u64,
+                                            args[i + 1],
+                                            10,
+                                        ),
+                                        else => args[i + 1],
+                                    };
+                                },
+                                else => return Err.Bruh,
+                            }
+                        }
+                    }
+                }
+            } else {
+                // no flags
             }
         }
+
+        var arguments = &.{};
+        std.debug.print("{any}\n", .{res});
+        return Result(flags){
+            .flags = res,
+            .arguments = arguments,
+        };
+    }
+
+    pub fn run(self: *Self) !void {
+        const args = try std.process.argsAlloc(self.allocator);
+        defer std.process.argsFree(self.allocator, args);
+
+        for (args) |arg| {
+            if (self.middlewares.get(arg)) |del| {
+                return del();
+            }
+        }
+
+        // no args matched to command
+        if (self.middlewares.get(noCommandFound)) |del| {
+            del();
+        }
+    }
+};
+
+// aliases type would be of the format
+//  {
+//    .help = .{"h", "hel"}
+//  }
+fn extractFlag(flag: u8, comptime flags: type) ?[]const u8 {
+    inline for (flags.aliases.kvs) |kv| {
+        if (kv.value == flag) {
+            return kv.key;
+        }
+    }
+
+    return null;
+}
+
+fn isDefaulted(comptime flags: type) bool {
+    comptime for (@typeInfo(flags).Struct.fields) |field| {
+        if (field.default_value == null) {
+            return false;
+        }
+    } else {
+        return true;
     };
 }
